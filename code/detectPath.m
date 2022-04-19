03e6dfe411cc8a3acb86fc7e0c8d9bf9860ac2af
SuperSpecialSweet <sxy1066740232@gmail.com>
2022-04-19 15:08:07 +0800

    detectpath

---
+++ code/detectPath.m
@@ -0,0 +1,13 @@
+function ConnPath = detectPath()
+    % detect Connpath on my own computer
+    ConnPath = "";
+
+    if getenv("USERNAME") == "Sxing"
+        ConnPath = 'C:/Users/Sxing/OneDrive/2021_2/matlab/Connectivity/';
+    elseif getenv("USER") == "sxing"
+        ConnPath = '/Users/sxing/Library/CloudStorage/OneDrive-Personal/2021_2/matlab/Connectivity/';
+    else
+        throw(MException("detect:error", "Connectivity not found"));
+    end
+
+end
