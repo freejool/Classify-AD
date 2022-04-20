770c22a357aefbc185f4b58541ac27f597c8d230
SuperSpecialSweet <sxy1066740232@gmail.com>
2022-04-20 10:58:22 +0800

    added venv

---
+++ code/K-Means/main.py
@@ -0,0 +1,42 @@
+from pyparsing import nums
+from sklearn.cluster import KMeans
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from mpl_toolkits.mplot3d import Axes3D
+# Importing the dataset
+dataset = pd.read_csv('test.csv')
+data = dataset.iloc[:, 2:5].values
+numSamples = len(data)
+corrctnum = []
+
+# 假如我要构造一个聚类数为3的聚类器
+estimator = KMeans(n_clusters=3)  # 构造聚类器
+estimator.fit(data)  # 聚类
+label_pred = estimator.labels_  # 获取聚类标签
+centroids = estimator.cluster_centers_  # 获取聚类中心
+inertia = estimator.inertia_  # 获取聚类准则的总和
+
+mark = ['or', 'ob', 'og']
+figure = plt.figure()
+ax = Axes3D(figure)
+for i in range(numSamples):
+    if dataset.iloc[i, :]['category'] == 'AD':
+        ax.scatter(data[i][0], data[i][1], data[i][2], c='r',)
+    else:
+        ax.scatter(data[i][0], data[i][1], data[i][2], c='b',)
+ax.legend(loc='best')
+mark = ['Dr', 'Db', 'Dg']
+for i in range(2):
+    plt.plot(centroids[i][0], centroids[i][1],
+             centroids[i][2], mark[i], markersize=12)
+plt.show()
+for i in range(2):
+    corrctnum.append(0)
+    for j in range(numSamples):
+        if estimator.labels_[j] == i and dataset.iloc[j, :]['category'] == 'AD':
+            corrctnum[-1] += 1
+        elif estimator.labels_[j] == 1-i and dataset.iloc[j, :]['category'] != 'AD':
+            corrctnum[-1] += 1
+print(max(corrctnum)/numSamples)
+pass
--- code/combine.py
+++ code/combine.py
@@ -1,23 +1,25 @@
 import collections
+import json
 from pprint import pprint
 import pymongo
-from bson.son import SON
 
+with open('code/config.json', 'r') as f:
+    config = json.load(f)
 
-client = pymongo.MongoClient(
-db=client.AD
-clusters=db.list_collection_names()
+client = pymongo.MongoClient(config['mongodburl'])
+db = client.AD
+clusters = db.list_collection_names()
 clusters.remove('test')
 clusters.remove('category')
-cursorIdx=db.category.find({})
-docs=[]
+cursorIdx = db.category.find({})
+docs = []
 for c in cursorIdx:
-    idx=c['index']
-    doc={'index': idx}
+    idx = c['index']
+    doc = {'index': idx}
     for clu in clusters:
-        data=db.get_collection(clu).find_one({'index': idx})
-        doc[clu]=data['value']
-    data=db.get_collection('category').find_one({'index': idx})
-    doc['category']=data['category']
+        data = db.get_collection(clu).find_one({'index': idx})
+        doc[clu] = data['value']
+    data = db.get_collection('category').find_one({'index': idx})
+    doc['category'] = data['category']
     docs.append(doc)
 db.test.insert_many(docs)
